447fbfe49a5dedf149227476e78f20cd44b6d846
ramchaulagain625 <ramchaulagain625@gmail.com>
2021-07-11 20:37:35 -0400

    Added few methods

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,4 +1,20 @@
 package Pages;
 
-public class LoginPage {
+import Utils.CommonMehtods;
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends CommonMehtods {
+    @FindBy(id="txtUsername")
+    public WebElement userNameBox;
+    @FindBy(id="txtPassword")
+    public WebElement passwordBox;
+    @FindBy(id="btnLogin")
+    public WebElement loginBtn;
+    public LoginPage(){
+        PageFactory.initElements(driver,this);
+    }
+
 }
--- src/test/java/Utils/CommonMehtods.java
+++ src/test/java/Utils/CommonMehtods.java
@@ -1,44 +1,60 @@
 package Utils;
 
-import io.github.bonigarcia.wdm.Config;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
-@Test
+import java.util.concurrent.TimeUnit;
+
+
 public class CommonMehtods {
 
-public static WebDriver driver;
-static String path;
+    public static WebDriver driver;
+    static String path;
 
 
-public static void launchBrowser(){
-    path = Constants.CONFIG_FILEPATH;
-    ConfigsReader.readProperties(path);
-     String browser =  ConfigsReader.getPropertyValue("browser");
-    switch (browser){
-        case "chrome":
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-            break;
-        case "firefox":
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
-            break;
-        default:
-            System.out.println("choose either firefox or chrome browser");
+    public static void launchBrowser() {
+        path = Constants.CONFIG_FILEPATH;
+        ConfigsReader.readProperties(path);
+        String browser = ConfigsReader.getPropertyValue("browser");
+        switch (browser) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            default:
+                System.out.println("choose either firefox or chrome browser");
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Constants.IMPLICITWAIT, TimeUnit.SECONDS);
+        driver.get(ConfigsReader.getPropertyValue("url"));
     }
-    driver.get( ConfigsReader.getPropertyValue( "url" ) );
-}
 
-
-
-public static void tearDown(){
-    if(driver!=null){
-        driver.quit();
+    public static void sendText(WebElement element, String text) {
+        element.clear();
+        element.sendKeys(text);
     }
-}
+    public static void explicitTimeWait(WebElement element){
+        WebDriverWait wait = new WebDriverWait(driver, Constants.EXPLICITWAIT);
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+    public static void click(WebElement element){
+        element.click();
+    }
+
+    public static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 
 }
